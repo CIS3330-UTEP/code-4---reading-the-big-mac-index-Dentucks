e7999c9a207428a0a7fd935933ae1239655ffc61
Dentucks <dennisged2014@gmail.com>
2024-09-25 16:28:40 -0600

    just started working on my code

--- code_4.py
+++ code_4.py
@@ -3,16 +3,36 @@ import pandas as pd
 big_mac_file = './big-mac-full-index.csv'
 
 def get_big_mac_price_by_year(year,country_code):
-    pass # Remove this line and code your function
+    df = pd.read_csv('./big-mac-full-index.csv')
 
 def get_big_mac_price_by_country(country_code):
-    pass # Remove this line and code your function
+    df = pd.read_csv('./big-mac-full-index.csv')
+    Cou_Code = df.query("iso_a3.str.lower() == @country_code.lower()")
+    # den = print(Cou_Code[['iso_a3']])
+    den_price = round(Cou_Code['dollar_price'].mean(),2)
+    return den_price
+
+    
 
 def get_the_cheapest_big_mac_price_by_year(year):
-    pass # Remove this line and code your function
+    df = pd.read_csv('./big-mac-full-index.csv')
+    yr_query =
+    min =
+    result = ('Malaysia(MYS): $%.1f' % (min))
 
 def get_the_most_expensive_big_mac_price_by_year(year):
-    pass # Remove this line and code your function
+    df = pd.read_csv('./big-mac-full-index.csv')
+    yr_query =
+    max =
+    result = ('Malaysia(MYS): $%.1f' % (max))
+    return result
 
 if __name__ == "__main__":
-    pass # Remove this line and code your user interface
+    mac1 = get_big_mac_price_by_year(2008,'rus')
+    print(mac1)
+    mac2 = get_big_mac_price_by_country("rus")
+    print(mac2)
+    mac3 = get_the_cheapest_big_mac_price_by_year()
+    print(mac3)
+    mac4 = get_the_most_expensive_big_mac_price_by_year()
+    print(mac4)
