7ca7105222b52400a3ec8437915ebea7314b2816
Dentucks <dennisged2014@gmail.com>
2024-10-01 18:06:08 -0600

    # I use ChatGPT trying to get year which i did but ended up messing up my whole code forgeting to use query so my code did't work, I try to fellow the steps and couldn't figure it out

--- code_4.py
+++ code_4.py
@@ -1,7 +1,7 @@
 import csv
 import pandas as pd
 big_mac_file = './big-mac-full-index.csv'
-
+# I use ChatGPT trying to get year which i did but ended up messing up my whole code forgeting to use query so my code did't work
 def get_big_mac_price_by_year(year,country_code):
     df = pd.read_csv('./big-mac-full-index.csv')
     filtered_data = df[(df['iso_a3'].str.lower() == country_code.lower()) & (df['date'].str.contains(str(year)))]
@@ -29,18 +29,25 @@ def get_the_cheapest_big_mac_price_by_year(year):
     
 
 def get_the_most_expensive_big_mac_price_by_year(year):
-     df = pd.read_csv('./big-mac-full-index.csv')
-     yr_query =
-     max =
-     result = ('Malaysia(MYS): $%.1f' % (max))
-     return result
+    df = pd.read_csv('./big-mac-full-index.csv')
+    filtered_data = df[df['date'].str.contains(str(year))]
+    if not filtered_data.empty:
+        max_price_row = filtered_data.loc[filtered_data['dollar_price'].idxmax()]
+        result = f"{max_price_row['name']}({max_price_row['iso_a3']}): ${max_price_row['dollar_price']:.1f}"
+        return result
+    else:
+        return f"No data available for the year {year}"
+     
 
 if __name__ == "__main__":
     mac1 = get_big_mac_price_by_year(2008,'rus')
     print(mac1)
+
     mac2 = get_big_mac_price_by_country("rus")
     print(mac2)
-    # mac3 = get_the_cheapest_big_mac_price_by_year(2008)
-    # print(mac3)
-    # mac4 = get_the_most_expensive_big_mac_price_by_year(2014)
-    # print(mac4)
+
+    mac3 = get_the_cheapest_big_mac_price_by_year(2008)
+    print(mac3)
+
+    mac4 = get_the_most_expensive_big_mac_price_by_year(2014)
+    print(mac4)
